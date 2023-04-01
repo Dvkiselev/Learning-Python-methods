023c027300886d3acdf96ed834ab393c9ed6a9b0
Dvkiselev <127286981+Dvkiselev@users.noreply.github.com>
2023-04-01 14:06:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import Def
 import random
 import textwrap as tw
 
-
 # ⏬ создание БД 
 
 with sqlite3.connect('function.db') as db:
 	cursor = db.cursor()
-	cursor.execute("""CREATE TABLE IF NOT          EXISTS function (
+	cursor.execute("""CREATE TABLE IF NOT EXISTS function (
 		type TEXT, 
 		method TEXT,
 		arguments TEXT,
-		description TEXT
-	)""")
+		description TEXT)""")
 	db.commit()
 
 Def.info()
@@ -22,21 +20,19 @@ print('\v')
 while True:
     
     # ⏬ Выбор действия
-
     
     Action_add_or_teach =input('\v\tДля добавления нового метода\n\tВведите:1\n\n\tДля тренировки\n\tВведите: 2\n\n\t')
     
     # ⏬ Добавление нового метода в БД 
 
     if Action_add_or_teach == '1':
-	    Def.Data_recording()
+        Def.Data_recording()
 	
-
     # ⏬ Тренировка 
 
     if Action_add_or_teach == '2':
     	while True:
-    		Def.vizov() 
+            Def.vizov() 
     			
 					  	        			  	        	
 
