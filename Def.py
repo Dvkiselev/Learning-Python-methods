82ab38f635ccd5f088e7cc11f526732c126a3a5a
Dvkiselev <127286981+Dvkiselev@users.noreply.github.com>
2023-08-01 23:04:48 +0300

    Update Def.py

--- Def.py
+++ Def.py
@@ -1,79 +1,56 @@
+import os
 import sqlite3
-import random
-import textwrap as tw
-	
-			
-def info():
-	'''
-	Метод открывает файл Readme.txt
-	для ознакомления с работой программы
-	'''
-	    
-	with open('Readme.txt', 'r') as f:
-		for line in f:
-			print(f'\t{line}')
 
-#----------------------------------------------				
-def Data_recording ():	
-	
-	'''
-	Функция дает пользователю ввести 
-	данные для записи в БД function.db
-	'''
-	
-	Type = input('Введите тип данных: ')
-	Function = input('Введите метод: ')
-	Arguments = input('Введите аргументы функции: ')
-	Description = input('Введите описание функции: ')
-		
-	# список введенных данных
-				
-	values = [type, method ,Arguments, description]
-	    
-	# запись данных в таблицу function.db
-	    
-	with sqlite3.connect('function.db') as db:
-		cursor = db.cursor()
-		cursor.execute("""INSERT INTO function 
-		(type, 
-		 method, 
-		 arguments, 
-		 description) VALUES(?, ?, ?, ?)""",values)
-		
-		db.commit()
-			
-	print('\v\tФункция добавленна')
-	
-#----------------------------------------------		
-def vizov ():	 
-    	
-	Type_vizov = input('\v\tВведите тип метода:\n\t_list_\t_str_\t_dict_\t_set_\t_tuple_\n\t_sql_')
-	
-	'''
-	Функция выводит из базы данных 
-	кортеж  выбранных элементов 
-	'''			
-						
-	with sqlite3.connect('function.db') as db:
-			cursor = db.cursor()
-			rows = cursor.execute(
-			f"SELECT * FROM function WHERE type LIKE '{Type_vizov}'"). fetchall() 
-	stroka = random.choice(rows)
-	#взять один элемент из кортежа
-		
-	print(f'\t\v{stroka[:2]}')
-	input()
-	print(f'\t\v{stroka[2]}') 
-	input()
-	print(f'\v{tw.fill(stroka[3], 30,)}')
-	input()
-	р
+        
+def creating_table():
 
-					
-		
-		
-	
-		
-	
-		
-		
+    '''
+    Создает базу данных
+    '''
+    
+    name_db = input('Напишите название новой БД: ')
+    name = input('напишите название полей через запятую:\n')
+    # создание полей
+    name = ','.join([i + ' TEXT' for i in name.split(',')])    
+    # создание БД
+    with sqlite3.connect(f'{name_db}.db') as db: 
+             cursor = db.cursor() 
+             cursor.execute(f"""CREATE TABLE IF NOT EXISTS function ({name}, status)""") 
+             db.commit() 
+
+
+def choosing_db ():
+    
+    '''
+    вывод всех баз данных в директории и выбор одной из них
+    '''
+    
+    name_file = [x for x in os.listdir() if x.startswith('.db', -3) == True]
+    print('выберете базу данных')
+    print(*enumerate(name_file), sep = '\n')
+    nam_db = int(input('введите номер БД -> '))
+    return name_file[nam_db]
+
+
+
+def data_entry_in_db():
+    
+    '''
+    Добавление данных
+    '''
+    
+    name_db = choosing_db()
+    with sqlite3.connect(f'{name_db}') as db: 
+        cursor = db.cursor() 8
+        # получение полей таблицы
+        cursor.execute("""SELECT* FROM function""")
+        colnames = cursor.description
+        list_colnames = [i[0] for i in colnames]
+        # ввод значений 
+        val = tuple(input(f'введите значенте {i}: ') for i in list_colnames)
+        #добавлнрие значений
+        cursor.execute(f"""INSERT INTO function  VALUES {val}""")
+    return 'Данные добавлены'
+        
+     
+   
